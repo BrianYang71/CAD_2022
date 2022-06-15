ac9cbf29d03baddf19fc554e5721a01a4dba33b6
KevinYang71 <b09901175@ntu.edu.tw>
2022-06-15 22:22:06 +0800

    Parser_verilog deleted.

--- Parser-Verilog/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/program1/eda/Parser-Verilog
-# Build directory: /mnt/c/program1/eda/Parser-Verilog/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(regression "/mnt/c/program1/eda/Parser-Verilog/unittest/regression")
